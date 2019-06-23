eba781c4668ee0ac8bf1467eebc2a5dc420151be
CallaJun <calla.carter@yahoo.com>
2019-06-23 11:56:00 -0400

    Further improved solution

--- week1/flipping-an-image.java
+++ week1/flipping-an-image.java
@@ -1,10 +1,8 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] A) {
-        int rowSize = A.length;
-        int colSize = A[0].length;
-        for (int i = 0; i < rowSize; i++) {
+        for (int i = 0; i < A.length; i++) {
             int lo = 0;
-            int hi = rowSize - 1;
+            int hi = A.length - 1;
             while (lo <= hi) {
                 int temp = A[i][lo] ^ 1;
                 A[i][lo] = A[i][hi] ^ 1;
