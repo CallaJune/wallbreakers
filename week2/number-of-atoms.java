a39460447e42438e976cf4e1606fe92f6df43ce6
CallaJun <calla.carter@yahoo.com>
2019-07-06 19:08:11 -0400

    Edit

--- week2/number-of-atoms.java
+++ week2/number-of-atoms.java
@@ -1,139 +1,47 @@
 // Uses the multiset class in Multiset.java
-// 14/28 test cases
 class Solution {
     public String countOfAtoms(String formula) {
-        Multiset<String> elements = parseFormula(formula);
-        // Create a sorted array of the elements in elements.elementSet()
-        String[] items = new String[elements.elementSet().size()];
-        int i = 0;
-        for (String item : elements.elementSet()) {
-            items[i++] = item;
-        }
-        Arrays.sort(items);
-        StringBuilder result = new StringBuilder();
-        // For each item in array, append item, count to result
-        for (String item : items) {
-            int count = elements.count(item);
-            if (count == 1) {
-                result.append(item);
+        Multiset<String> map = new Multiset<>();
+        Stack<Integer> factors = new Stack<>();
+        
+        int count = 1;
+        int factor = 1;
+        String cstr = "";
+        
+        // Iterate backward
+        for (int i = formula.length() - 1; i >= 0; i--) {
+            char c = formula.charAt(i);
+            if (Character.isUpperCase(c)) {
+                count = cstr.isEmpty() ? 1 : Integer.parseInt(cstr);
+                String element = String.valueOf(c);
+                map.add(element, count * factor);
+                cstr = "";
+            } else if (Character.isLowerCase(c)) {
+                count = cstr.isEmpty() ? 1 : Integer.parseInt(cstr);
+                String element = formula.charAt(--i) + "" + c;
+                map.add(element, count * factor);
+                cstr = "";
+            } else if (Character.isDigit(c)) {
+                cstr = c + cstr;
+            } else if (c == ')') {
+                count = Integer.parseInt(cstr);
+                factor *= count;
+                factors.push(count);
+                cstr = "";
             } else {
-                result.append(item + count);   
+                factor /= factors.pop();
             }
+        }
+
+        List<String> s = new ArrayList<>();
+        s.addAll(map.elementSet());
+
+        // Sort list alphabetically
+        Collections.sort(s);
+        StringBuilder result = new StringBuilder();
+        for (String str : s) {
+            result.append(str + (map.count(str) > 1 ? map.count(str) : ""));
         }
         return result.toString();
-    }
-    
-    public Multiset<String> parseFormula(String formula) {
-        Multiset<String> elements = new Multiset<>();
-        Stack<Multiset<String>> stack = new Stack<>();
-        Multiset<String> map = new Multiset<>();
-        StringBuilder s = new StringBuilder();
-        // Iterate over formula
-        int i = 0;
-        int numParens = 0;
-        while (i < formula.length()) {
-            char c = formula.charAt(i);
-            if (c == '(') {
-                if (s.length() > 0) {
-                    // Put current atom onto map
-                    map.add(s.toString());
-                    // Reset atom string builder s
-                    s.setLength(0);   
-                }
-                // Put current map onto stack, empty map
-                if (map.size() > 0) {
-                    stack.push(map); 
-                }
-                map = new Multiset<String>(); 
-                i++;
-            } else if (c == ')') {
-                // Clear s and if it exists
-                if (s.length() > 0) {
-                    // Put current atom onto map
-                    map.add(s.toString());
-                    // Reset atom string builder s
-                    s.setLength(0);
-                }
-                // Put current map onto stack, empty map
-                if (map.size() > 0) {
-                    stack.push(map);
-                }
-                map = new Multiset<String>();
-                numParens++;
-                i++;
-            } else if (Character.isDigit(c)) {
-                // If s not empty, capture value
-                String atom = s.toString();
-                s.setLength(0);
-                int num = 0;
-                // Build number
-                while (i < formula.length() && Character.isDigit(formula.charAt(i))) {
-                    s.append(formula.charAt(i));
-                    i++;
-                }
-                num = Integer.parseInt(s.toString());
-                s.setLength(0);
-                // If s not empty, add s to map
-                if (atom.length() > 0) {
-                    // Put current atom onto map
-                    map.add(atom, num);
-                    // Clear s
-                    s.setLength(0);
-                } else {
-                    ArrayList<Multiset<String>> list = new ArrayList<Multiset<String>>();
-                    for (int p = 0; p < numParens; p++) {
-                        // Multiply everything in stack top by num
-                        if (stack.empty()) {
-                            continue;
-                        }
-                        Multiset<String> mapToIncrement = stack.pop();
-                        for (String atomName : mapToIncrement.elementSet()) {
-                            mapToIncrement.setCount(atomName, 0, mapToIncrement.count(atomName) * num);
-                        }
-                        list.add(mapToIncrement);
-                    }
-                    // Push everything on arraylist to stack in reverse order
-                    for (int it = list.size() - 1; it >= 0; it--) {
-                        stack.push(list.get(it));
-                    }
-                }
-            } else if (Character.isUpperCase(c)) {
-                if (s.length() > 0) {
-                    // Put current atom onto map
-                    map.add(s.toString());
-                    // Reset atom string builder s
-                    s.setLength(0);   
-                }
-                // Initialize s with current
-                s.append(c);
-                i++;
-            } else if (Character.isLowerCase(c)) {
-                s.append(c);
-                i++;
-            }
-        }
-        
-        // Add any trailing atom names
-        if (s.length() > 0) {
-            map.add(s.toString());
-            s.setLength(0);
-        }
-        
-        // Add contents of map to elements
-        if (map.size() > 0) {
-            for (String atomName : map.elementSet()) {
-                elements.add(atomName, map.count(atomName));
-            }
-        }
-        
-        // Merge all multisets into elements
-        while (!stack.empty()) {
-            Multiset<String> setToMerge = stack.pop();
-            for (String atomName : setToMerge.elementSet()) {
-                elements.add(atomName, setToMerge.count(atomName));
-            }
-        }
-        
-        return elements;
     }
 }
