81a2d8490eb593de24f03487a82e8cf2950c05ae
Calla <calla.carter@yahoo.com>
2019-07-09 14:29:25 -0400

    Improved

--- week3/minimum-number-of-arrows-to-burst-balloons.java
+++ week3/minimum-number-of-arrows-to-burst-balloons.java
@@ -3,26 +3,26 @@ class Solution {
         if (points.length <= 1) {
             return points.length;
         }
-        // Sort points by starting x-value
-        Arrays.sort(points, Comparator.comparingInt(arr -> arr[0]));
-        // Set current to first interval in points
-        int[] current = points[0];
+        // Sort points by ending x-value
+        Arrays.sort(points, Comparator.comparingInt(arr -> arr[1]));
+        // Set current to first interval's end in points
+        int current = points[0][1];
         // Point next to second item in points
         int next = 1;
         int result = 1;
         // Iterate over intervals in points
         while (next < points.length) {
             // Check if current and next overlap
-            if (current[1] >= points[next][0]) {
-                current[0] = Math.max(current[0], points[next][0]);
-                current[1] = Math.min(current[1], points[next][1]);
-            } else {
-                // Increment result
-                result++;
-                // Set current to next
-                current = points[next];
+            if (current >= points[next][0]) {
+                next++;
+                continue;
             }
-            next++; 
+            // Set current to next
+            current = points[next][1];
+            // Increment result
+            result++;
+            // Increment next
+            next++;
         }
         return result;
     }
