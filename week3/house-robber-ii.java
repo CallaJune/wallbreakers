3c884aebd6f97a8215d86ffc05970f4fa6563be2
CallaJun <calla.carter@yahoo.com>
2019-07-14 14:57:18 -0400

    Added recursive solution

--- week3/house-robber-ii.java
+++ week3/house-robber-ii.java
@@ -11,25 +11,28 @@ class Solution {
         return Math.max(robHouses(Arrays.copyOfRange(nums, 0, nums.length - 1)), robHouses(Arrays.copyOfRange(nums, 1, nums.length)));
     }
 
-    // Recursive solution to House Robber:
-    // https://leetcode.com/problems/house-robber/description/
-    public int robHouses(int[] nums) {
-        return robHouse(nums, nums.length - 1);
-    }
-    
     // Recursive solution to House Robber - helper function:
     // https://leetcode.com/problems/house-robber/description/
-    public int robHouse(int[] nums, int n) {
-        if (n < 0) {
+    public int robHouses(int[] nums) {
+        if (nums.length == 0) {
             return 0;
-        } else if (n == 0) {
+        } else if (nums.length == 1) {
             return nums[0];
-        } else if (n == 1) {
-            return Math.max(nums[0], nums[1]);
         }
-        // Choose current house and house two houses away
-        // or previous house
-        return Math.max(nums[n] + robHouse(nums, n - 2), robHouse(nums, n - 1));
+        int[] mem = new int[nums.length];
+        mem[0] = nums[0];
+        mem[1] = Math.max(nums[0], nums[1]);
+        return robHouses(nums, mem, 2);
+    }
+    
+    public int robHouses(int[] nums, int[] mem, int n) {
+        if (n == nums.length) {
+            return mem[mem.length - 1];
+        } else {
+            int max = Math.max(nums[n] + mem[n - 2], mem[n - 1]);
+            mem[n] = max;
+            return robHouses(nums, mem, n + 1);
+        }
     }
     
     // Dynamic programming solution to House Robber:
