97342517866b73670f80b37223d158a05e925aa7
CallaJun <calla.carter@yahoo.com>
2020-03-23 15:53:21 -0400

    Ran autoformatter

--- spring2020/unit1/valid-anagram.py
+++ spring2020/unit1/valid-anagram.py
@@ -1,7 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return self.charCounts(s) == self.charCounts(t)
-    
+
     def charCounts(self, s: str) -> dict:
         d = {}
         for c in s:
